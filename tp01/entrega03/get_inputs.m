8023877c1c84f1ad7bef4ab91aba58aa38430f66
Macarse <macarse@gmail.com>
2011-04-03 15:57:28 -0300

    Added bias to the pattern

--- tp01/entrega03/get_inputs.m
+++ tp01/entrega03/get_inputs.m
@@ -3,9 +3,9 @@ function ret = get_inputs()
     ret = []; 
 
     pos = 1;
-    for i = 0:0.001:4
+    for i = 0:0.01:4
 
-        ret(pos).pattern = i;
+		ret(pos).pattern = [-1 i];
         ret(pos).output = func(i);
         pos = pos+1;
     end
