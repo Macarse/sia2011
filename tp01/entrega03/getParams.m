9bbeb91b047e0bbc363ce479cf4d0d79e9e877d6
ITBA <user@hashy.(none)>
2011-04-04 13:06:22 -0300

    agrego la exponencial

--- tp01/entrega03/getParams.m
+++ tp01/entrega03/getParams.m
@@ -1,24 +1,31 @@
 function params = getParams()
     params.beta = 1;
-    params.g = inline(sprintf('tanh(%f*x)', params.beta));
-    params.g_diff = inline(sprintf('(%f) * (1 - %s.^2)', params.beta, formula(params.g)));
+    
+    % tanh
+    %params.g = inline(sprintf('tanh(%f*x)', params.beta));
+    %params.g_diff = inline(sprintf('(%f) * (1 - %s.^2)', params.beta, formula(params.g)));
+    
+    % exp
+    params.g = inline(sprintf('1./(1+exp(%f.*x))',-2*params.beta));
+    params.g_diff = inline(sprintf('2*%f.*(%s).*(1-%s)', params.beta, formula(params.g), formula(params.g)));
+    
 
     params.m = 1;
     params.repeat = 0; % set to 0 to deactivate pattern repetition
-    params.step = 0.01;
+    params.step = 0.03;
 
     params.MAX_WEIGHT = 0.5;
     params.MIN_WEIGHT = -0.5;
 
     params.INPUTS_QTY = 1;
-    params.hidden = [15,15];
+    params.hidden = [13,13];
     params.OUTPUTS_QTY = 1;
 
-    params.N_EPOCHS = 20000;
-    params.tolerancia = 10^(-2);
+    params.N_EPOCHS = 5000;
+    params.tolerancia = 0.5;
 
     % set to 0.0 to use no gradient padding improvement
-    params.gradient_padding = 0.0;
+    params.gradient_padding = 0.01;
 
     % -- ADAPTATIVE ETA --
     params.eta = 0.02;
@@ -32,6 +39,6 @@ function params = getParams()
 
     % -- momentum --
     % set to 0.0 to use no momentum
-    params.momentum_alpha = 0.5;
+    params.momentum_alpha = 0.1;
 
 end
